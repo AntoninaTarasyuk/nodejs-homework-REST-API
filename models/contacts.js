8ec30c6dd6de5e1c26bdf1a0622c18e1e75b5fe7
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-10-30 09:37:20 +0200

    add code in models/contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,46 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.resolve('./models/contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf8');
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactById = contacts.find(contact => contact.id === contactId);
+  if (!contactById) { return null };
+  return contactById;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactForRemove = await getContactById();
+  const newData = contacts.filter(contact => contact.id !== contactId);
+  fs.writeFile(contactsPath, JSON.stringify(newData));
+  return contactForRemove;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {id: uuidv4(), name, email, phone}
+  const newData = contacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(newData));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contactIdx = contacts.findIndex(contact => contact.id === contactId);
+  if (contactIdx === -1) { return };
+  const contactForUpdate = contacts[contactIdx];
+  Object.assign(contactForUpdate, body);
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return contactForUpdate;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +48,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
