6f2c6afc90bfdd492cc12ef0584d278617273435
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-10-30 13:47:43 +0200

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,8 +4,8 @@ const {
   listContacts,
   getContactById,
   removeContact,
-  // addContact,
-  // updateContact,
+  addContact,
+  updateContact,
 } = require('../../models/contacts');
 
 router.get('/', async (req, res) => {
@@ -34,30 +34,47 @@ router.get('/:contactId', async (req, res) => {
 router.delete('/:contactId', async (req, res) => {
   try {
     const { contactId } = req.params;
-    const isContactDeleted = await removeContact(contactId);
-    if (!isContactDeleted) {
+    const deletedContact = await removeContact(contactId);
+    if (!deletedContact) {
       res.status(404).json({ code: '404', message: 'Not found' });
       return;
     }
-    res.status(200).json({ code: '200', message: 'Successfully removed', isContactDeleted });
+    res.status(200).json({ code: '200', message: 'Successfully removed', deletedContact });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
   try {
     const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+      res.status(400).json({ code: '400', message: 'Missing required field' });
+      return;
+    }
     const newContact = await addContact(name, email, phone);
-    
     res.status(201).json({ code: '201', message: 'Created', newContact });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', async (req, res) => {
+  try {
+    const { contactId } = req.params;
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ code: '400', message: 'Missing required field' });
+      return;
+    }
+    const updatedContact = await updateContact(contactId, req.body);
+    if (!updatedContact) {
+      res.status(404).json({ code: '404', message: 'Not found' });
+      return;
+    }
+    res.status(200).json({ code: '200', message: 'Successfully updated', updatedContact });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-module.exports = router
+module.exports = router;
