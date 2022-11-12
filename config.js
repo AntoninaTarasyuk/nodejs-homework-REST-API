d6570dded244fd2a20a36ea06a994eb9177191e8
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-11-12 12:48:25 +0200

    fix removeContact model

---
+++ config.js
@@ -0,0 +1,6 @@
+const HOST_DB = 'mongodb+srv://Mary:FIrrldn8xcXt0MWs@cluster0.nfkabqj.mongodb.net/contactsDB?retryWrites=true&w=majority';
+const PORT = '3000';
+module.exports = {
+  HOST_DB,
+  PORT,
+};
